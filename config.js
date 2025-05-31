e6412d3625fb243cea88e4d7a59f28a639f22d30
shehan1342 <101106511+shehan1342@users.noreply.github.com>
2025-05-31 14:05:56 +0530

    update config.js

--- config.js
+++ config.js
@@ -2,4 +2,6 @@ module.exports = {
   SESSION_ID: "PRABATH-MD~U7tDxQyb#Pqtf0fW_tjA3SfeaqzqGTFZbgOzDJH4CwsmKCkaIIHw",
   OPENAI_API_KEY: "your_openai_api_key_here",
   MONGO_URL: "mongodb+srv://pancha:2006.Shehan@cluster0.hyugfei.mongodb.net/",
+  prefix: "!", // ← මෙතනින් ඔබට ඕන symbol එක set කරන්න (!, ., / etc.)
+  
 };
