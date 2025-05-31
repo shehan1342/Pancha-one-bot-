4103c112eeed514226053e618cf1ee5ba8388a55
shehan1342 <101106511+shehan1342@users.noreply.github.com>
2025-05-31 12:46:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: "PRABATH-MD~U7tDxQyb#Pqtf0fW_tjA3SfeaqzqGTFZbgOzDJH4CwsmKCkaIIHw",
   OPENAI_API_KEY: "your_openai_api_key_here",
-  MONGO_URL: "mongodb+srv://your_mongo_connection_url",
+  MONGO_URL: "mongodb+srv://pancha:2006.Shehan@cluster0.hyugfei.mongodb.net/",
 };
